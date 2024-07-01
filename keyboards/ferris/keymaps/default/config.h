a2dfd3abe7635df8dc0c3108f09cb704ab4c36a8
Alex Su <choyuansu@gmail.com>
2024-07-01 09:35:02 -0700

    aaa

--- keyboards/ferris/keymaps/default/config.h
+++ keyboards/ferris/keymaps/default/config.h
@@ -34,8 +34,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Pick good defaults for enabling homerow modifiers
 #define TAPPING_TERM 200
-#define PERMISSIVE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS
 #define QUICK_TAP_TERM 0
+
+#define COMBO_TERM 40
 
 // Underglow configuration
 #ifdef RGBLIGHT_ENABLE
